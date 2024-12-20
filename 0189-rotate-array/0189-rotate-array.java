7bf3778296f62f85e9c3f260caf4e74dccf0edfb
saiprabhu-dandanayak <saiprabhu.dandanayak@zemosolabs.com>
2024-12-20 16:35:09 +0530

    Time: 0 ms (100.00%), Space: 57.7 MB (9.18%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int length=nums.length;
+
+      if(k>length)
+        k=k%length;
+      reverse(nums, 0, length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, length - 1);
+       
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
